cba60d601527777f6ac5c4b9dafcbacb87a388a0
aa <123456>
2017-09-26 19:41:50 +0800

    债权信息接口

--- src/main/java/com/qianfeng/controller/T_Debt_InfoController.java
+++ src/main/java/com/qianfeng/controller/T_Debt_InfoController.java
@@ -5,6 +5,7 @@ import com.qianfeng.entity.T_Debt_Info;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.annotation.Resource;
 import java.util.List;
@@ -12,13 +13,16 @@ import java.util.List;
 /**
  * Created by Administrator on 2017/9/26.fanzy
  */
-@Controller("/T_Debt_Info")
+@Controller
+@RequestMapping("/T_Debt_Info")
 public class T_Debt_InfoController {
     @Resource
     private T_Debt_Info_Dao debt_info_Dao;
 
-    @RequestMapping(value = "all",method = RequestMethod.GET)
+    @RequestMapping(value = "all")
+    @ResponseBody
     public List<T_Debt_Info> findAlLDebtInfo(){
+        System.out.println("---");
         return debt_info_Dao.findAllT_Debt_Info();
     }
 }
--- src/main/java/com/qianfeng/entity/T_Debt_Info.java
+++ src/main/java/com/qianfeng/entity/T_Debt_Info.java
@@ -2,8 +2,6 @@ package com.qianfeng.entity;
 
 import java.io.Serializable;
 import java.math.BigDecimal;
-import java.sql.Timestamp;
-import java.util.Arrays;
 
 /**
  * Created by Administrator on 2017/9/25.fanzy
@@ -20,7 +18,7 @@ public class T_Debt_Info implements Serializable{
     private float d_debt_money;//债权金额
     private float d_debt_month_rate;//债权月利率
     private String d_debt_no;//债权编号
-    private Timestamp d_debt_ransfer_out_date;//债权转出日期
+    private String d_debt_ransfer_out_date;//债权转出日期
     private BigDecimal d_debt_status;//债权状态
     private String d_debt_status_name;//债权状态名字
     private String d_debt_transferred_date;//债权转入日期
@@ -31,10 +29,10 @@ public class T_Debt_Info implements Serializable{
     private String d_debt_year_rate;//债权年化利率
     private String d_debtors_id;//债务人身份证号
     private String d_debtors_name;//债务人名称
-    private Timestamp d_loan_end_date;//原始借款到期日期
+    private String d_loan_end_date;//原始借款到期日期
     private BigDecimal d_loan_period;//原始借款期限
     private String d_loan_purpose;//借款用途
-    private Timestamp d_loan_start_date;//原始借款开始日期
+    private String d_loan_start_date;//原始借款开始日期
     private String d_loan_type;//借款类型
     private float d_matched_money;//已匹配金额
     private BigDecimal d_matched_status;//匹配状态
@@ -44,43 +42,7 @@ public class T_Debt_Info implements Serializable{
     private BigDecimal d_repayment_style;//还款方式
     private String d_repayment_style_name;//还款方式名称
 
-    public T_Debt_Info(BigDecimal d_id, float d_availble_money, BigDecimal d_availble_period, BigDecimal d_borrower_id, String d_contract_no, String d_creditor, float d_debt_money, float d_debt_month_rate, String d_debt_no, Timestamp d_debt_ransfer_out_date, BigDecimal d_debt_status, String d_debt_status_name, String d_debt_transferred_date, float d_debt_transferred_money, BigDecimal d_debt_transferred_period, String d_debt_type, String d_debt_type_name, String d_debt_year_rate, String d_debtors_id, String d_debtors_name, Timestamp d_loan_end_date, BigDecimal d_loan_period, String d_loan_purpose, Timestamp d_loan_start_date, String d_loan_type, float d_matched_money, BigDecimal d_matched_status, String d_matched_status_name, String d_repaymen_date, float d_repaymen_money, BigDecimal d_repayment_style, String d_repayment_style_name) {
-        this.d_id = d_id;
-        this.d_availble_money = d_availble_money;
-        this.d_availble_period = d_availble_period;
-        this.d_borrower_id = d_borrower_id;
-        this.d_contract_no = d_contract_no;
-        this.d_creditor = d_creditor;
-        this.d_debt_money = d_debt_money;
-        this.d_debt_month_rate = d_debt_month_rate;
-        this.d_debt_no = d_debt_no;
-        this.d_debt_ransfer_out_date = d_debt_ransfer_out_date;
-        this.d_debt_status = d_debt_status;
-        this.d_debt_status_name = d_debt_status_name;
-        this.d_debt_transferred_date = d_debt_transferred_date;
-        this.d_debt_transferred_money = d_debt_transferred_money;
-        this.d_debt_transferred_period = d_debt_transferred_period;
-        this.d_debt_type = d_debt_type;
-        this.d_debt_type_name = d_debt_type_name;
-        this.d_debt_year_rate = d_debt_year_rate;
-        this.d_debtors_id = d_debtors_id;
-        this.d_debtors_name = d_debtors_name;
-        this.d_loan_end_date = d_loan_end_date;
-        this.d_loan_period = d_loan_period;
-        this.d_loan_purpose = d_loan_purpose;
-        this.d_loan_start_date = d_loan_start_date;
-        this.d_loan_type = d_loan_type;
-        this.d_matched_money = d_matched_money;
-        this.d_matched_status = d_matched_status;
-        this.d_matched_status_name = d_matched_status_name;
-        this.d_repaymen_date = d_repaymen_date;
-        this.d_repaymen_money = d_repaymen_money;
-        this.d_repayment_style = d_repayment_style;
-        this.d_repayment_style_name = d_repayment_style_name;
-    }
-
     public T_Debt_Info() {
-
     }
 
     public BigDecimal getD_id() {
@@ -155,11 +117,11 @@ public class T_Debt_Info implements Serializable{
         this.d_debt_no = d_debt_no;
     }
 
-    public Timestamp getD_debt_ransfer_out_date() {
+    public String getD_debt_ransfer_out_date() {
         return d_debt_ransfer_out_date;
     }
 
-    public void setD_debt_ransfer_out_date(Timestamp d_debt_ransfer_out_date) {
+    public void setD_debt_ransfer_out_date(String d_debt_ransfer_out_date) {
         this.d_debt_ransfer_out_date = d_debt_ransfer_out_date;
     }
 
@@ -243,11 +205,11 @@ public class T_Debt_Info implements Serializable{
         this.d_debtors_name = d_debtors_name;
     }
 
-    public Timestamp getD_loan_end_date() {
+    public String getD_loan_end_date() {
         return d_loan_end_date;
     }
 
-    public void setD_loan_end_date(Timestamp d_loan_end_date) {
+    public void setD_loan_end_date(String d_loan_end_date) {
         this.d_loan_end_date = d_loan_end_date;
     }
 
@@ -267,11 +229,11 @@ public class T_Debt_Info implements Serializable{
         this.d_loan_purpose = d_loan_purpose;
     }
 
-    public Timestamp getD_loan_start_date() {
+    public String getD_loan_start_date() {
         return d_loan_start_date;
     }
 
-    public void setD_loan_start_date(Timestamp d_loan_start_date) {
+    public void setD_loan_start_date(String d_loan_start_date) {
         this.d_loan_start_date = d_loan_start_date;
     }
 
@@ -339,42 +301,41 @@ public class T_Debt_Info implements Serializable{
         this.d_repayment_style_name = d_repayment_style_name;
     }
 
-    @Override
-    public String toString() {
-        return "T_Debt_Info{" +
-                "d_id=" + d_id +
-                ", d_availble_money=" + d_availble_money +
-                ", d_availble_period=" + d_availble_period +
-                ", d_borrower_id=" + d_borrower_id +
-                ", d_contract_no='" + d_contract_no + '\'' +
-                ", d_creditor='" + d_creditor + '\'' +
-                ", d_debt_money=" + d_debt_money +
-                ", d_debt_month_rate=" + d_debt_month_rate +
-                ", d_debt_no='" + d_debt_no + '\'' +
-                ", d_debt_ransfer_out_date=" + d_debt_ransfer_out_date +
-                ", d_debt_status=" + d_debt_status +
-                ", d_debt_status_name='" + d_debt_status_name + '\'' +
-                ", d_debt_transferred_date='" + d_debt_transferred_date + '\'' +
-                ", d_debt_transferred_money=" + d_debt_transferred_money +
-                ", d_debt_transferred_period=" + d_debt_transferred_period +
-                ", d_debt_type='" + d_debt_type + '\'' +
-                ", d_debt_type_name='" + d_debt_type_name + '\'' +
-                ", d_debt_year_rate='" + d_debt_year_rate + '\'' +
-                ", d_debtors_id='" + d_debtors_id + '\'' +
-                ", d_debtors_name='" + d_debtors_name + '\'' +
-                ", d_loan_end_date=" + d_loan_end_date +
-                ", d_loan_period=" + d_loan_period +
-                ", d_loan_purpose='" + d_loan_purpose + '\'' +
-                ", d_loan_start_date=" + d_loan_start_date +
-                ", d_loan_type='" + d_loan_type + '\'' +
-                ", d_matched_money=" + d_matched_money +
-                ", d_matched_status=" + d_matched_status +
-                ", d_matched_status_name='" + d_matched_status_name + '\'' +
-                ", d_repaymen_date='" + d_repaymen_date + '\'' +
-                ", d_repaymen_money=" + d_repaymen_money +
-                ", d_repayment_style=" + d_repayment_style +
-                ", d_repayment_style_name='" + d_repayment_style_name + '\'' +
-                '}';
+    public T_Debt_Info(BigDecimal d_id, float d_availble_money, BigDecimal d_availble_period, BigDecimal d_borrower_id, String d_contract_no, String d_creditor, float d_debt_money, float d_debt_month_rate, String d_debt_no, String d_debt_ransfer_out_date, BigDecimal d_debt_status, String d_debt_status_name, String d_debt_transferred_date, float d_debt_transferred_money, BigDecimal d_debt_transferred_period, String d_debt_type, String d_debt_type_name, String d_debt_year_rate, String d_debtors_id, String d_debtors_name, String d_loan_end_date, BigDecimal d_loan_period, String d_loan_purpose, String d_loan_start_date, String d_loan_type, float d_matched_money, BigDecimal d_matched_status, String d_matched_status_name, String d_repaymen_date, float d_repaymen_money, BigDecimal d_repayment_style, String d_repayment_style_name) {
+        this.d_id = d_id;
+        this.d_availble_money = d_availble_money;
+        this.d_availble_period = d_availble_period;
+        this.d_borrower_id = d_borrower_id;
+        this.d_contract_no = d_contract_no;
+        this.d_creditor = d_creditor;
+        this.d_debt_money = d_debt_money;
+        this.d_debt_month_rate = d_debt_month_rate;
+        this.d_debt_no = d_debt_no;
+        this.d_debt_ransfer_out_date = d_debt_ransfer_out_date;
+        this.d_debt_status = d_debt_status;
+        this.d_debt_status_name = d_debt_status_name;
+        this.d_debt_transferred_date = d_debt_transferred_date;
+        this.d_debt_transferred_money = d_debt_transferred_money;
+        this.d_debt_transferred_period = d_debt_transferred_period;
+        this.d_debt_type = d_debt_type;
+        this.d_debt_type_name = d_debt_type_name;
+        this.d_debt_year_rate = d_debt_year_rate;
+        this.d_debtors_id = d_debtors_id;
+        this.d_debtors_name = d_debtors_name;
+        this.d_loan_end_date = d_loan_end_date;
+        this.d_loan_period = d_loan_period;
+        this.d_loan_purpose = d_loan_purpose;
+        this.d_loan_start_date = d_loan_start_date;
+        this.d_loan_type = d_loan_type;
+        this.d_matched_money = d_matched_money;
+        this.d_matched_status = d_matched_status;
+        this.d_matched_status_name = d_matched_status_name;
+        this.d_repaymen_date = d_repaymen_date;
+        this.d_repaymen_money = d_repaymen_money;
+        this.d_repayment_style = d_repayment_style;
+        this.d_repayment_style_name = d_repayment_style_name;
+
+
     }
 }
 /* BigDecimal  String  String
@@ -387,10 +348,10 @@ public class T_Debt_Info implements Serializable{
    D_DEBT_MONEY         float comment '债权金额',
    D_DEBT_MONTH_RATE    float comment '债权月利率',
    D_DEBT_NO            varchar(255) comment '债权编号',
-   D_DEBT_RANSFER_OUT_DATE Timestamptamp comment '债权转出日期',
+   D_DEBT_RANSFER_OUT_DATE Stringtamp comment '债权转出日期',
    D_DEBT_STATUS        numeric(10,0) comment '债权状态',
    D_DEBT_STATUS_NAME   varchar(255) comment '债权状态名字',
-   D_DEBT_TRANSFERRED_DATE Timestamptamp comment '债权转入日期',
+   D_DEBT_TRANSFERRED_DATE Stringtamp comment '债权转入日期',
    D_DEBT_TRANSFERRED_MONEY float comment '债权转入金额',
    D_DEBT_TRANSFERRED_PERIOD numeric(10,0) comment '债权转入期限',
    D_DEBT_TYPE          varchar(255) comment '标的类型',
@@ -398,10 +359,10 @@ public class T_Debt_Info implements Serializable{
    D_DEBT_YEAR_RATE     float comment '债权年化利率',
    D_DEBTORS_ID         varchar(255) comment '债务人身份证号',
    D_DEBTORS_NAME       varchar(255) comment '债务人名称',
-   D_LOAN_END_DATE      Timestamptamp comment '原始借款到期日期',
+   D_LOAN_END_DATE      Stringtamp comment '原始借款到期日期',
    D_LOAN_PERIOD        numeric(10,0) comment '原始借款期限',
    D_LOAN_PURPOSE       varchar(255) comment '借款用途',
-   D_LOAN_START_DATE    Timestamptamp comment '原始借款开始日期',
+   D_LOAN_START_DATE    Stringtamp comment '原始借款开始日期',
    D_LOAN_TYPE          varchar(255) comment '借款类型',
    D_MATCHED_MONEY      float comment '已匹配金额',
    D_MATCHED_STATUS     numeric(10,0) comment '匹配状态',
